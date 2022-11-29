cc2f1868b98188f138f981af21a5fcbbdd13f0f9
Matthias Pfeil <m.pfeil@reedu.de>
2022-11-29 12:05:08 +0100

    Add umami tracking

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html className="bg-zinc-900">
+      <Head>
+        <script
+          async
+          defer
+          data-website-id="127fbeb9-f91d-4cdf-a92c-d6f62a968582"
+          src="https://teixeira.opensensemap.org/umami.js"
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
