7fbfc4787abadaa9f9be60b2be91f52384f58992
Matthias Pfeil <m.pfeil@reedu.de>
2022-12-12 16:27:07 +0100

    Use unified with remakr and rehype

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,19 +1,55 @@
-import { remark } from 'remark'
-import html from 'remark-html'
-import gfm from 'remark-gfm'
-import remark2rehype from 'remark-rehype'
-import rehypeVideo from 'rehype-video'
-import stringify from 'rehype-stringify'
+import { unified } from 'unified'
+import remarkGfm from 'remark-gfm'
+import remarkParse from 'remark-parse'
+import remarkDirective from 'remark-directive'
+import remarkRehype from 'remark-rehype'
+import rehypeFormat from 'rehype-format'
+import rehypeSanitize, { defaultSchema } from 'rehype-sanitize'
+import rehypeRaw from 'rehype-raw'
+import rehypeStringify from 'rehype-stringify'
+import { visit } from 'unist-util-visit'
+import { MdastRoot } from 'remark-rehype/lib'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark()
-    .use(gfm)
-    // .use(html, {
-    //   sanitize: true,
-    // })
-    .use(remark2rehype, { allowDangerousHtml: true })
-    .use(rehypeVideo, { details: false })
-    .use(stringify)
+  const result = await unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkDirective)
+    .use(remarkYoutube)
+    .use(remarkRehype)
+    .use(rehypeRaw)
+    .use(rehypeStringify)
     .process(markdown)
-  return result.toString()
+  return String(result)
+}
+
+// This plugin is an example to turn `::youtube` into iframes.
+/** @type {import('unified').Plugin<[], import('mdast').Root>} */
+function remarkYoutube() {
+  return (tree: MdastRoot) => {
+    visit(tree, node => {
+      if (
+        node.type === 'textDirective' ||
+        node.type === 'leafDirective' ||
+        node.type === 'containerDirective'
+      ) {
+        if (node.name !== 'youtube') return
+
+        const data = node.data || (node.data = {})
+        const attributes = node.attributes || {}
+        const id = attributes.id
+
+        data.hName = 'iframe'
+        data.hProperties = {
+          class: 'yt-video',
+          src: 'https://www.youtube.com/embed/' + id,
+          width: 200,
+          height: 200,
+          frameBorder: 0,
+          allow: 'picture-in-picture',
+          allowFullScreen: true,
+        }
+      }
+    })
+  }
 }
